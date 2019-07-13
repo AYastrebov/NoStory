0d9fa2228b4b969492da3d12dd3c7e762d41e216
Andrey Yastrebov <ayastrebov@gmail.com>
2019-07-13 20:08:08 +0300

    Initial Commit

---
+++ NoStory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStory
+//
+//  Created by Andrey Yastrebov on 13/07/2019.
+//  Copyright © 2019 Andrey Yastrebov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
